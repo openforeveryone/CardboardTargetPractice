74702875e21a163a4eace0de90cb27c72e30c64b
MattW <matt@openforeveryone.co.uk>
2016-01-22 16:46:25 +0000

    Video rendering classes added and working.

--- app/src/main/java/com/ofemobile/targetvr/MainActivity.java
+++ app/src/main/java/com/ofemobile/targetvr/MainActivity.java
@@ -28,10 +28,20 @@ import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
 import android.graphics.RectF;
+import android.media.MediaCodec;
+import android.media.MediaCodecInfo;
+import android.media.MediaFormat;
+import android.media.MediaMuxer;
+import android.opengl.EGL14;
+import android.opengl.EGLContext;
+import android.opengl.EGLDisplay;
+import android.opengl.EGLExt;
+import android.opengl.EGLSurface;
 import android.opengl.GLES20;
 import android.opengl.GLUtils;
 import android.opengl.Matrix;
 import android.os.Bundle;
+import android.os.Environment;
 import android.os.Handler;
 import android.os.Looper;
 import android.os.Vibrator;
@@ -39,8 +49,10 @@ import android.text.Layout;
 import android.text.StaticLayout;
 import android.text.TextPaint;
 import android.util.Log;
+import android.view.Surface;
 
 import java.io.BufferedReader;
+import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
@@ -177,6 +189,14 @@ public class MainActivity extends CardboardActivity implements CardboardView.Ste
   private float beamDist = 0;
   boolean beamHit = false;
   int flareStartFrame = -51;
+
+  //EGL state for renderer:
+  EGLDisplay mScreenEglDisplay;
+  EGLSurface mScreenEglDrawSurface;
+  EGLSurface mScreenEglReadSurface;
+  EGLContext mScreenEglContext;
+
+  private VideoEncoder mVideoEncoder;
 
 
   /**
@@ -525,6 +545,20 @@ public class MainActivity extends CardboardActivity implements CardboardView.Ste
     show3DToast("Find the target cube then pull the magnet", 10000);
 
     checkGLError("onSurfaceCreated");
+
+    //Backup the context
+    mScreenEglDisplay = EGL14.eglGetCurrentDisplay();
+    mScreenEglDrawSurface = EGL14.eglGetCurrentSurface(EGL14.EGL_DRAW);
+    mScreenEglReadSurface = EGL14.eglGetCurrentSurface(EGL14.EGL_READ);
+    mScreenEglContext = EGL14.eglGetCurrentContext();
+
+    //Setup the video surface and encoder
+    mVideoEncoder = new VideoEncoder();
+    mVideoEncoder.prepare(320, 240, 2000000, mScreenEglContext);
+
+    //Restore the rendering context
+    EGL14.eglMakeCurrent(mScreenEglDisplay, mScreenEglDrawSurface,
+            mScreenEglReadSurface, mScreenEglContext);
   }
 
   /**
@@ -769,6 +803,30 @@ public class MainActivity extends CardboardActivity implements CardboardView.Ste
     }
 
     checkGLError("onReadyToDraw");
+
+    //Backup the screen context
+    mScreenEglDisplay = EGL14.eglGetCurrentDisplay();
+    mScreenEglDrawSurface = EGL14.eglGetCurrentSurface(EGL14.EGL_DRAW);
+    mScreenEglReadSurface = EGL14.eglGetCurrentSurface(EGL14.EGL_READ);
+    mScreenEglContext = EGL14.eglGetCurrentContext();
+
+    if (frameNo<=100) {
+      //Switch to the recording context
+      mVideoEncoder.inputSurface().makeCurrent();
+      mVideoEncoder.drain(false);
+      generateSurfaceFrame(frameNo, mVideoEncoder.width(), mVideoEncoder.height());
+      mVideoEncoder.inputSurface().swapBuffers();
+    }
+    if (frameNo==100)
+    {
+      mVideoEncoder.drain(true);
+      mVideoEncoder.release();
+      Log.i(TAG, "Recording Finished");
+    }
+
+    //Restore the screen context
+    EGL14.eglMakeCurrent(mScreenEglDisplay, mScreenEglDrawSurface,
+            mScreenEglReadSurface, mScreenEglContext);
   }
 
 
@@ -796,11 +854,41 @@ public class MainActivity extends CardboardActivity implements CardboardView.Ste
     show3DToast(message, messagetime);
   }
 
-  /**
-   * Draws a frame for an eye.
-   *
-   * @param eye The eye to render. Includes all required transformations.
-   */
+  // RGB color values for generated frames
+  private static final int TEST_R0 = 0;
+  private static final int TEST_G0 = 136;
+  private static final int TEST_B0 = 0;
+  private static final int TEST_R1 = 236;
+  private static final int TEST_G1 = 50;
+  private static final int TEST_B1 = 186;
+  private void generateSurfaceFrame(int frameIndex, int width, int height) {
+    frameIndex %= 8;
+
+    int startX, startY;
+    if (frameIndex < 4) {
+      // (0,0) is bottom-left in GL
+      startX = frameIndex * (width / 4);
+      startY = height / 2;
+    } else {
+      startX = (7 - frameIndex) * (width / 4);
+      startY = 0;
+    }
+
+    GLES20.glClearColor(TEST_R0 / 255.0f, TEST_G0 / 255.0f, TEST_B0 / 255.0f, 1.0f);
+    GLES20.glClear(GLES20.GL_COLOR_BUFFER_BIT);
+
+    GLES20.glEnable(GLES20.GL_SCISSOR_TEST);
+    GLES20.glScissor(startX, startY, width / 4, height / 2);
+    GLES20.glClearColor(TEST_R1 / 255.0f, TEST_G1 / 255.0f, TEST_B1 / 255.0f, 1.0f);
+    GLES20.glClear(GLES20.GL_COLOR_BUFFER_BIT);
+    GLES20.glDisable(GLES20.GL_SCISSOR_TEST);
+  }
+
+    /**
+     * Draws a frame for an eye.
+     *
+     * @param eye The eye to render. Includes all required transformations.
+     */
   @Override
   public void onDrawEye(Eye eye) {
     GLES20.glEnable(GLES20.GL_DEPTH_TEST);
@@ -1346,6 +1434,350 @@ public class MainActivity extends CardboardActivity implements CardboardView.Ste
   private void updateReticle(int type) {
     reticleUpdater.setType(type);
     mainLoopHandler.post(reticleUpdater);
+  }
+
+
+  private static class VideoEncoder {
+
+    // encoder / muxer state
+    private MediaCodec mEncoder;
+    private MediaMuxer mMuxer;
+    private int mTrackIndex;
+    private boolean mMuxerStarted;
+    private CodecInputSurface mInputSurface;
+    private int mWidth = -1;
+    private int mHeight = -1;
+    private int mBitRate = -1;
+    // parameters for the encoder
+    private static final String MIME_TYPE = "video/avc";    // H.264 Advanced Video Coding
+    private static final int FRAME_RATE = 15;               // 15fps
+    private static final int IFRAME_INTERVAL = 10;          // 10 seconds between I-frames
+    private static final int NUM_FRAMES = 30;               // two seconds of video
+
+    private static final File OUTPUT_DIR = Environment.getExternalStorageDirectory();
+
+    // allocate one of these up front so we don't need to do it every time
+    private MediaCodec.BufferInfo mBufferInfo;
+
+    private void prepare(int width, int height, int bitRate, EGLContext shareContext) {
+      mWidth=width;
+      mHeight=height;
+      mBitRate=bitRate;
+      mBufferInfo = new MediaCodec.BufferInfo();
+      EGLContext recordContext;
+
+      MediaFormat format = MediaFormat.createVideoFormat(MIME_TYPE, mWidth, mHeight);
+
+      // Set some properties.  Failing to specify some of these can cause the MediaCodec
+      // configure() call to throw an unhelpful exception.
+      format.setInteger(MediaFormat.KEY_COLOR_FORMAT,
+              MediaCodecInfo.CodecCapabilities.COLOR_FormatSurface);
+      format.setInteger(MediaFormat.KEY_BIT_RATE, mBitRate);
+      format.setInteger(MediaFormat.KEY_FRAME_RATE, FRAME_RATE);
+      format.setInteger(MediaFormat.KEY_I_FRAME_INTERVAL, IFRAME_INTERVAL);
+//      if (VERBOSE) Log.d(TAG, "format: " + format);
+
+      // Create a MediaCodec encoder, and configure it with our format.  Get a Surface
+      // we can use for input and wrap it with a class that handles the EGL work.
+      //
+      // If you want to have two EGL contexts -- one for display, one for recording --
+      // you will likely want to defer instantiation of CodecInputSurface until after the
+      // "display" EGL context is created, then modify the eglCreateContext call to
+      // take eglGetCurrentContext() as the share_context argument.
+      try {
+        mEncoder = MediaCodec.createEncoderByType(MIME_TYPE);
+      } catch (IOException e) {
+        throw new RuntimeException("MediaCodec createEncoderByType failed", e);
+      }
+
+//            Log.i(TAG, "onSurfaceCreated context:" + shareContext);
+
+      mEncoder.configure(format, null, null, MediaCodec.CONFIGURE_FLAG_ENCODE);
+      mInputSurface = new CodecInputSurface(mEncoder.createInputSurface(), shareContext);
+      mEncoder.start();
+
+      recordContext = mInputSurface.mEGLContext;
+      Log.i(TAG, "recordContext created:" + recordContext);
+
+      // Output filename.  Ideally this would use Context.getFilesDir() rather than a
+      // hard-coded output directory.
+      String outputPath = new File(OUTPUT_DIR,
+              "test." + mWidth + "x" + mHeight + ".mp4").toString();
+      Log.d(TAG, "output file is " + outputPath);
+
+
+      // Create a MediaMuxer.  We can't add the video track and start() the muxer here,
+      // because our MediaFormat doesn't have the Magic Goodies.  These can only be
+      // obtained from the encoder after it has started processing data.
+      //
+      // We're not actually interested in multiplexing audio.  We just want to convert
+      // the raw H.264 elementary stream we get from MediaCodec into a .mp4 file.
+      try {
+        mMuxer = new MediaMuxer(outputPath, MediaMuxer.OutputFormat.MUXER_OUTPUT_MPEG_4);
+      } catch (IOException ioe) {
+        throw new RuntimeException("MediaMuxer creation failed", ioe);
+      }
+
+      mTrackIndex = -1;
+      mMuxerStarted = false;
+    }
+
+    /**
+     * Extracts all pending data from the encoder.
+     * <p>
+     * If endOfStream is not set, this returns when there is no more data to drain.  If it
+     * is set, we send EOS to the encoder, and then iterate until we see EOS on the output.
+     * Calling this with endOfStream set should be done once, right before stopping the muxer.
+     */
+    private void drain(boolean endOfStream) {
+      final int TIMEOUT_USEC = 10000;
+//      if (VERBOSE) Log.d(TAG, "drainEncoder(" + endOfStream + ")");
+
+      if (endOfStream) {
+//        if (VERBOSE) Log.d(TAG, "sending EOS to encoder");
+        mEncoder.signalEndOfInputStream();
+      }
+
+      ByteBuffer[] encoderOutputBuffers = mEncoder.getOutputBuffers();
+      while (true) {
+        int encoderStatus = mEncoder.dequeueOutputBuffer(mBufferInfo, TIMEOUT_USEC);
+        if (encoderStatus == MediaCodec.INFO_TRY_AGAIN_LATER) {
+          // no output available yet
+          if (!endOfStream) {
+            break;      // out of while
+          } else {
+//            if (VERBOSE) Log.d(TAG, "no output available, spinning to await EOS");
+          }
+        } else if (encoderStatus == MediaCodec.INFO_OUTPUT_BUFFERS_CHANGED) {
+          // not expected for an encoder
+          encoderOutputBuffers = mEncoder.getOutputBuffers();
+        } else if (encoderStatus == MediaCodec.INFO_OUTPUT_FORMAT_CHANGED) {
+          // should happen before receiving buffers, and should only happen once
+          if (mMuxerStarted) {
+            throw new RuntimeException("format changed twice");
+          }
+          MediaFormat newFormat = mEncoder.getOutputFormat();
+          Log.d(TAG, "encoder output format changed: " + newFormat);
+
+          // now that we have the Magic Goodies, start the muxer
+          mTrackIndex = mMuxer.addTrack(newFormat);
+          mMuxer.start();
+          mMuxerStarted = true;
+        } else if (encoderStatus < 0) {
+          Log.w(TAG, "unexpected result from encoder.dequeueOutputBuffer: " +
+                  encoderStatus);
+          // let's ignore it
+        } else {
+          ByteBuffer encodedData = encoderOutputBuffers[encoderStatus];
+          if (encodedData == null) {
+            throw new RuntimeException("encoderOutputBuffer " + encoderStatus +
+                    " was null");
+          }
+
+          if ((mBufferInfo.flags & MediaCodec.BUFFER_FLAG_CODEC_CONFIG) != 0) {
+            // The codec config data was pulled out and fed to the muxer when we got
+            // the INFO_OUTPUT_FORMAT_CHANGED status.  Ignore it.
+//            if (VERBOSE) Log.d(TAG, "ignoring BUFFER_FLAG_CODEC_CONFIG");
+            mBufferInfo.size = 0;
+          }
+
+          if (mBufferInfo.size != 0) {
+            if (!mMuxerStarted) {
+              throw new RuntimeException("muxer hasn't started");
+            }
+
+            // adjust the ByteBuffer values to match BufferInfo (not needed?)
+            encodedData.position(mBufferInfo.offset);
+            encodedData.limit(mBufferInfo.offset + mBufferInfo.size);
+
+            mMuxer.writeSampleData(mTrackIndex, encodedData, mBufferInfo);
+//            if (VERBOSE) Log.d(TAG, "sent " + mBufferInfo.size + " bytes to muxer");
+          }
+
+          mEncoder.releaseOutputBuffer(encoderStatus, false);
+
+          if ((mBufferInfo.flags & MediaCodec.BUFFER_FLAG_END_OF_STREAM) != 0) {
+            if (!endOfStream) {
+              Log.w(TAG, "reached end of stream unexpectedly");
+            } else {
+//              if (VERBOSE) Log.d(TAG, "end of stream reached");
+            }
+            break;      // out of while
+          }
+        }
+      }
+    }
+
+    /**
+     * Releases encoder resources.  May be called after partial / failed initialization.
+     */
+    private void release() {
+//      if (VERBOSE) Log.d(TAG, "releasing encoder objects");
+      if (mEncoder != null) {
+        mEncoder.stop();
+        mEncoder.release();
+        mEncoder = null;
+      }
+      if (mInputSurface != null) {
+        mInputSurface.release();
+        mInputSurface = null;
+      }
+      if (mMuxer != null) {
+        mMuxer.stop();
+        mMuxer.release();
+        mMuxer = null;
+      }
+    }
+
+    public void setCodecInputSurface (CodecInputSurface inputSurface) {
+      mInputSurface=inputSurface;
+    }
+
+    public int width() {
+      return mWidth;
+    }
+
+    public int height() {
+      return mHeight;
+    }
+    public CodecInputSurface inputSurface() {
+      return mInputSurface;
+    }
+  }
+  /**
+   * Holds state associated with a Surface used for MediaCodec encoder input.
+   * <p>
+   * The constructor takes a Surface obtained from MediaCodec.createInputSurface(), and uses that
+   * to create an EGL window surface.  Calls to eglSwapBuffers() cause a frame of data to be sent
+   * to the video encoder.
+   * <p>
+   * This object owns the Surface -- releasing this will release the Surface too.
+   */
+  private static class CodecInputSurface {
+    private static final int EGL_RECORDABLE_ANDROID = 0x3142;
+
+    private EGLDisplay mEGLDisplay = EGL14.EGL_NO_DISPLAY;
+    public EGLContext mEGLContext = EGL14.EGL_NO_CONTEXT;
+    private EGLSurface mEGLSurface = EGL14.EGL_NO_SURFACE;
+
+    private Surface mSurface;
+
+    /**
+     * Creates a CodecInputSurface from a Surface.
+     */
+    public CodecInputSurface(Surface surface, EGLContext displayContext) {
+      if (surface == null) {
+        throw new NullPointerException();
+      }
+      mSurface = surface;
+
+      eglSetup(displayContext);
+    }
+
+    /**
+     * Prepares EGL.  We want a GLES 2.0 context and a surface that supports recording.
+     */
+    private void eglSetup(EGLContext displayContext) {
+      mEGLDisplay = EGL14.eglGetDisplay(EGL14.EGL_DEFAULT_DISPLAY);
+      if (mEGLDisplay == EGL14.EGL_NO_DISPLAY) {
+        throw new RuntimeException("unable to get EGL14 display");
+      }
+      int[] version = new int[2];
+      if (!EGL14.eglInitialize(mEGLDisplay, version, 0, version, 1)) {
+        throw new RuntimeException("unable to initialize EGL14");
+      }
+
+      // Configure EGL for recording and OpenGL ES 2.0.
+      int[] attribList = {
+              EGL14.EGL_RED_SIZE, 8,
+              EGL14.EGL_GREEN_SIZE, 8,
+              EGL14.EGL_BLUE_SIZE, 8,
+              EGL14.EGL_ALPHA_SIZE, 8,
+              EGL14.EGL_RENDERABLE_TYPE, EGL14.EGL_OPENGL_ES2_BIT,
+              EGL_RECORDABLE_ANDROID, 1,
+              EGL14.EGL_NONE
+      };
+      android.opengl.EGLConfig[] configs = new android.opengl.EGLConfig[1];
+      int[] numConfigs = new int[1];
+      EGL14.eglChooseConfig(mEGLDisplay, attribList, 0, configs, 0, configs.length,
+              numConfigs, 0);
+      checkEglError("eglCreateContext RGB888+recordable ES2");
+
+      // Configure context for OpenGL ES 2.0.
+      int[] attrib_list = {
+              EGL14.EGL_CONTEXT_CLIENT_VERSION, 2,
+              EGL14.EGL_NONE
+      };
+      mEGLContext = EGL14.eglCreateContext(mEGLDisplay, configs[0], displayContext,
+              attrib_list, 0);
+      checkEglError("eglCreateContext");
+
+      // Create a window surface, and attach it to the Surface we received.
+      int[] surfaceAttribs = {
+              EGL14.EGL_NONE
+      };
+      mEGLSurface = EGL14.eglCreateWindowSurface(mEGLDisplay, configs[0], mSurface,
+              surfaceAttribs, 0);
+      checkEglError("eglCreateWindowSurface");
+    }
+
+    /**
+     * Discards all resources held by this class, notably the EGL context.  Also releases the
+     * Surface that was passed to our constructor.
+     */
+    public void release() {
+      if (mEGLDisplay != EGL14.EGL_NO_DISPLAY) {
+        EGL14.eglMakeCurrent(mEGLDisplay, EGL14.EGL_NO_SURFACE, EGL14.EGL_NO_SURFACE,
+                EGL14.EGL_NO_CONTEXT);
+        EGL14.eglDestroySurface(mEGLDisplay, mEGLSurface);
+        EGL14.eglDestroyContext(mEGLDisplay, mEGLContext);
+        EGL14.eglReleaseThread();
+        EGL14.eglTerminate(mEGLDisplay);
+      }
+
+      mSurface.release();
+
+      mEGLDisplay = EGL14.EGL_NO_DISPLAY;
+      mEGLContext = EGL14.EGL_NO_CONTEXT;
+      mEGLSurface = EGL14.EGL_NO_SURFACE;
+
+      mSurface = null;
+    }
+
+    /**
+     * Makes our EGL context and surface current.
+     */
+    public void makeCurrent() {
+      EGL14.eglMakeCurrent(mEGLDisplay, mEGLSurface, mEGLSurface, mEGLContext);
+      checkEglError("eglMakeCurrent");
+    }
+
+    /**
+     * Calls eglSwapBuffers.  Use this to "publish" the current frame.
+     */
+    public boolean swapBuffers() {
+      boolean result = EGL14.eglSwapBuffers(mEGLDisplay, mEGLSurface);
+      checkEglError("eglSwapBuffers");
+      return result;
+    }
+
+    /**
+     * Sends the presentation time stamp to EGL.  Time is expressed in nanoseconds.
+     */
+    public void setPresentationTime(long nsecs) {
+      EGLExt.eglPresentationTimeANDROID(mEGLDisplay, mEGLSurface, nsecs);
+      checkEglError("eglPresentationTimeANDROID");
+    }
+
+    /**
+     * Checks for EGL errors.  Throws an exception if one is found.
+     */
+    private void checkEglError(String msg) {
+      int error;
+      if ((error = EGL14.eglGetError()) != EGL14.EGL_SUCCESS) {
+        throw new RuntimeException(msg + ": EGL error: 0x" + Integer.toHexString(error));
+      }
+    }
   }
 
 }
